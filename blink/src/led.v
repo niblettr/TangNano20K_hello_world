c39cf8215201bfb11998c82d2d880c0469a93074
Rob Niblett <140264168+niblettr@users.noreply.github.com>
2025-02-20 16:02:58 +0000

    all leds are now in sync

--- blink/src/led.v
+++ blink/src/led.v
@@ -5,64 +5,53 @@ input Clock: Declares an input port named Clock.
 output IO_voltage: Declares an output port named IO_voltage.
 This module is designed to control the output voltage (IO_voltage) based on the input clock signal (Clock).
 */
-module led(
+module led( 
     input  Clock,
     output IO_voltage,
     output IO_voltage2
 );
 
-/********** Counter **********/
-// parameter Clock_frequency = 27_000_000; // Crystal oscillator frequency is 27MHz
-parameter count_value_05S = 13_5_999;//13_499_999; // The number of clock cycles needed to time 0.5 seconds
-parameter count_value_01S = 2_699_999;//13_499_999; // 26_699_999;
+    /********** Counter **********/
+    // parameter Clock_frequency = 27_000_000; // Crystal oscillator frequency is 27MHz
+    parameter count_value_05S = 13_499_999; // The number of clock cycles needed to time 0.5 seconds
+    parameter count_value_01S = 2_699_999;  // The number of clock cycles needed to time 0.1 seconds
 
-reg [23:0] count_value_reg; // Counter register (24 bits, large enough to store 13_499_999)
-//reg        count_value_flag; // IO change flag
+    reg [23:0] count_value_reg  = 24'b0; // Counter register (24 bits)
+    reg [23:0] count_value_reg2 = 24'b0; // Counter register (24 bits)
 
-reg [23:0] count_value_reg2; // Counter register (24 bits, large enough to store 13_499_999)
-//reg        count_value_flag2; // IO change flag
-/****************************************************************************************************/
-always @(posedge Clock) begin
-    // Counter for 0.5 seconds
-    if (count_value_reg <= count_value_05S) begin
-        count_value_reg <= count_value_reg + 1; // Increment counter
+    /****************************************************************************************************/
+    always @(posedge Clock) begin
+        // Counter for 0.5 seconds
+        if (count_value_reg < count_value_05S) begin
+            count_value_reg <= count_value_reg + 1; // Increment counter
+        end
+        else begin
+            count_value_reg <= 24'b0; // Reset counter
+        end
+
+        // Counter for 0.1 seconds
+        if (count_value_reg2 < count_value_01S) begin
+            count_value_reg2 <= count_value_reg2 + 1; // Increment counter
+        end
+        else begin
+            count_value_reg2 <= 24'b0; // Reset counter
+        end
     end
-    else begin
-        count_value_reg <= 24'b0; // Reset counter
+
+    /********** IO voltage flip **********/
+    reg IO_voltage_reg  = 1'b0; // Initial state
+    reg IO_voltage_reg2 = 1'b0; // Initial state
+
+    always @(posedge Clock) begin
+        /****************************************************************************************************/
+        IO_voltage_reg  <= IO_voltage_reg  ^ (count_value_reg  == 0);
+        IO_voltage_reg2 <= IO_voltage_reg2 ^ (count_value_reg2 == 0);
+        /****************************************************************************************************/
     end
 
-    // Counter for 0.1 seconds
-    if (count_value_reg2 <= count_value_01S) begin
-        count_value_reg2 <= count_value_reg2 + 1; // Increment counter
-    end
-    else begin
-        count_value_reg2 <= 24'b0; // Reset counter
-    end
-end
-
-/********** IO voltage flip **********/
-reg IO_voltage_reg = 1'b0; // Initial state
-reg IO_voltage_reg2 = 1'b0; // Initial state
-
-always @(posedge Clock) begin
-/****************************************************************************************************/
-    IO_voltage_reg <= IO_voltage_reg ^(count_value_reg == 0);
-    //if ( count_value_reg == 0)  //  Flip flag 
-        //IO_voltage_reg <= ~IO_voltage_reg; // IO voltage flip
-//    else
- //       IO_voltage_reg <= IO_voltage_reg; // IO voltage constant
-/****************************************************************************************************/
-      IO_voltage_reg2 <= IO_voltage_reg2 ^(count_value_reg2 == 0);
-    //if ( count_value_reg2 == 0 )  //  Flip flag 
-        //IO_voltage_reg2 <= ~IO_voltage_reg2; // IO voltage flip
-    //else
-        //IO_voltage_reg2 <= IO_voltage_reg2; // IO voltage constant
-/****************************************************************************************************/
-end
-
-/* Continuous Assignment: continuously drives the value of IO_voltage with the value of IO_voltage_reg*/
-assign IO_voltage = IO_voltage_reg;
-assign IO_voltage2 = IO_voltage_reg2;
+    /* Continuous Assignment: continuously drives the value of IO_voltage with the value of IO_voltage_reg */
+    assign IO_voltage  = IO_voltage_reg;
+    assign IO_voltage2 = IO_voltage_reg2;
 
 endmodule
 
