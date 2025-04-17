e9f4a046f8a1561f01a16e45d7e00857d619af7f
Rob Niblett <140264168+niblettr@users.noreply.github.com>
2025-04-17 12:23:54 +0100

    changed fifo read/write enable to be edge triggered

--- blink/src/fifo.v
+++ blink/src/fifo.v
@@ -19,11 +19,25 @@ module fifo #(
     reg [$clog2(DEPTH):0] read_ptr = 0;     // Read pointer
     reg [$clog2(DEPTH):0] count = 0;        // Count of elements in the FIFO
 
+    reg read_en_d;
+    reg write_en_d;
+
+    // Capture previous state of read_en & write_en
+    always @(posedge clock or posedge reset) begin
+        if (reset) begin
+            read_en_d  <= 1'b0;
+            write_en_d <= 1'b0;
+        end else begin
+            read_en_d  <= read_en;
+            write_en_d <= write_en;
+        end
+    end
+
     // Write operation
     always @(posedge clock or posedge reset) begin
         if (reset) begin           
            write_ptr <= 0;
-        end else if (write_en && !full) begin
+        end else if (write_en && !write_en_d && !full) begin
            mem[write_ptr] <= data_in;       // Write data to memory
            write_ptr <= write_ptr + 1'b1;  // Increment write pointer
            // or write_ptr <= (write_ptr + 1'b1) % DEPTH;  // Increment write pointer
@@ -34,7 +48,7 @@ module fifo #(
     always @(posedge clock or posedge reset) begin
         if (reset) begin
             read_ptr <= 0;
-        end else if (read_en && !empty) begin
+        end else if (read_en && !read_en_d && !empty) begin
             read_ptr <= read_ptr + 1'b1;    // Increment read pointer
             //or read_ptr <= (read_ptr + 1'b1) % DEPTH;    // Increment read pointer
             Debug_fifo <= ~Debug_fifo;
@@ -46,7 +60,7 @@ module fifo #(
         if (reset) begin
             count <= 0;
         end else begin
-            case ({write_en && !full, read_en && !empty})
+            case ({write_en && !write_en_d && !full, read_en && !read_en_d&& !empty})
                 2'b10: count <= count + 1'b1; // Write only
                 2'b01: count <= count - 1'b1; // Read only
                 default: count <= count;      // No change
