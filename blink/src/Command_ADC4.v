8e498b919e5b7ef530180a37046e4bf3a2d1f110
Rob Niblett <140264168+niblettr@users.noreply.github.com>
2025-06-11 13:50:15 +0100

    no message

---
+++ blink/src/Command_ADC4.v
@@ -0,0 +1,90 @@
+/*
+; mux_inp                   defines Analog Muxer channel
+; buf_addr                  defines address for storing adc output from 1. board
+; buf_addr + offset1        defines address for storing adc output from 2. board
+; buf_addr + offset2        defines address for storing adc output from 3. board
+; buf_addr + offset3        defines address for storing adc output from 4. board
+[pb_adc4_16] (mux_inp, buf_addr, offset1, offset2, offset3)
+(
+MOV     P1,#BOARD_ALL OR PORT_MUX OR CTR_OFF
+MOV     A,#%mux_inp
+;Send the required data to the multiplexer latches...
+MOVX    @R0,A                 ; output mux-port
+CLR     DIR_OUT               ; switch data on Phase Bus
+CLR     PB_WR                 ; SETB WR activ
+NOP                           ; make pulse longer
+SETB    PB_WR                 ; SETB WR passiv
+NOP                           ; wait until mux-out stable
+NOP
+NOP
+NOP
+;Start the ADC conversion now mux-out is stable...
+CLR     PB_WR                 ; SETB WR activ
+NOP                           ; make pulse longer
+SETB    PB_WR                 ; SETB WR passiv
+;Now read the data back from all the boards...
+SETB    DIR_OUT               ; Switch Phase Bus for input
+LOOP4
+MOV     P1,#BOARD_1 OR PORT_ADC_HIGH OR RD_ON
+MOV     DPTR,#%buf_addr
+MOVX    A,@R0                 ; read data from bus
+SETB    PB_RD                 ; read inactive
+MOVX    @DPTR,A               ; store data to DPR
+MOV     P1,#BOARD_1 OR PORT_ADC_LOW OR RD_ON
+INC     DPTR
+MOVX    A,@R0                 ; read data from bus
+SETB    PB_RD                 ; read inactive
+MOVX    @DPTR,A               ; store data to DPR
+GOTO LOOP4
+*/
+
+    // handles the pb_adc4_16 & pb_adc4_8
+    // doing pb_adc4_16 first
+    if (substate_pb_adc4_active) begin
+        case (substate_pb_adc4)
+            SUBSTATE_PB_ADC4_IDLE: begin
+                Board_ID_ptr <= 0;
+                data_dir <= DIR_INPUT;
+                wait_multiples <= 3;
+                substate_pb_adc4_next = SUBSTATE_PB_ADC4_DONE;
+                substate_pb_adc4 <= SUBSTATE_PB_ADC4_WAIT_750N;
+                end
+
+            //MOV     P1,#BOARD_ALL OR PORT_MUX OR CTR_OFF
+            //#define BOARD_ALL            0x05 // -----101
+            SUBSTATE_PB_ADC4_ASSERT_ADDRESS_ID: begin
+                BOARD_X <= 5;//BOARD_ALL;
+                AddessPortPin <= PORT_MUX;
+                WrP <= DISABLE; // CTR_OFF in the assembler
+                RdP <= DISABLE; // CTR_OFF in the assembler
+                wait_multiples <= 1;
+                substate_pb_adc4 <= SUBSTATE_PB_ADC4_WAIT_750N;
+                substate_pb_adc4_next <= SUBSTATE_PB_ADC4_DONE; // <<<<<<<<<<<<<<<<<<<<<<
+            end
+
+            SUBSTATE_PB_ADC4_WAIT_750N: begin
+                if(wait_multiples) begin
+                    if (substate_wait_counter < 21) begin // Proceed after 21 cycles (~777ns) if clock = 20MHZ, 750ns can be achieved
+                        substate_wait_counter <= substate_wait_counter + 1'b1;
+                    end else begin                        
+                        substate_wait_counter <= 0;
+                        wait_multiples <= wait_multiples - 3'd1;
+                    end
+                end else begin
+                   substate_pb_adc4 <= substate_pb_adc4_next;
+                end
+            end
+            SUBSTATE_PB_ADC4_DONE: begin
+                //if(CommandType == 0) begin
+                   ResponseBytes[0:3] <= Read_Data_buffer[0:3];
+                   ResponseByteCount  <= 4;
+                //end
+
+                substate_pb_adc4_complete <= 1'b1;   // Indicate substate_pb_adc4 completion
+                substate_pb_adc4 <= SUBSTATE_PB_ADC4_IDLE;
+            end
+
+        endcase
+    end else begin
+        substate_pb_adc4_complete <= 1'b0; // Clear the flag when substate_pb_i_read4 is inactive
+    end // end of if (substate_pb_i_read4_active) begin
