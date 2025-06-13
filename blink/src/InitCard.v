1c2b752646ef88da6e3e20a9ef1e157ca62a73d0
Rob Niblett <140264168+niblettr@users.noreply.github.com>
2025-06-13 08:25:16 +0100

    no message

--- blink/src/InitCard.v
+++ blink/src/InitCard.v
@@ -3,8 +3,8 @@
        LampResetPin   <= 1'b1;        // hold in reset
        data_dir       <= DIR_INPUT;
        Data_Out_Port  <= 8'b0;
-       AddessPort  <= 3'b0;
-       TestAddress   <= 1'b01;
+       AddessPort     <= 3'b0;
+       TestAddress    <= 1'b01;
        PB_RD          <= 1'b1;
        PB_WR          <= 1'b1;
     end else begin
